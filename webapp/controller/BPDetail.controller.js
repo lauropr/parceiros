7ebfc0426cf22d5fb756fa60818e8d83fa401f60
lauropribeiro@gmail.com <lauropribeiro@gmail.com>
2022-12-21 21:43:39 -0300

    Comportamento dos botoes/form

--- webapp/controller/BPDetail.controller.js
+++ webapp/controller/BPDetail.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("z00.parceiros.controller.BPDetail", {
             onInit: function () {
+
+                //Inicializa modelos locais
+                this.inicializarModelosLocais();                
 
                 //acessa o Roteador
                 let oRouter = this.getOwnerComponent().getRouter();
@@ -16,6 +20,24 @@ sap.ui.define([
                 //anexa uma função de popular detalhes quando a rota RouteBPDetail for processada
                 oRouter.getRoute("RouteBPDetail").attachPatternMatched(this.populaDadosDetalhe, this);
 
+
+            },
+
+            inicializarModelosLocais(){
+
+                //vincula modelo na view
+                let oView = this.getView();
+                
+                //inicializar o modelo que controla a propriedade editable dos Inputs através da propriedade /editavel
+                let oModeloInputs = new JSONModel();
+                oModeloInputs.setProperty("/editavel", false);
+                oView.setModel(oModeloInputs, "inputs");
+
+                //inicializar o modelo que controla a propriedade editable dos Inputs através da propriedade /editavel
+                let oModeloBotoes = new JSONModel();
+                oModeloBotoes.setProperty("/edicao", false);
+                oModeloBotoes.setProperty("/visualizacao", true);
+                oView.setModel(oModeloBotoes, "botoes");
 
             },
 
@@ -47,8 +69,34 @@ sap.ui.define([
 
                     //nova alteração
                 }
-            }
+            },
 
+            aoEditar: function(oEvent){
+
+                //resgata modelo inputs para alterar a propriedade /editavel
+                let oView = this.getView();
+
+                let oModeloInputs = oView.getModel("inputs");
+                oModeloInputs.setProperty("/editavel", true);
+
+                let oModeloBotoes = oView.getModel("botoes");
+                oModeloBotoes.setProperty("/edicao", true);
+                oModeloBotoes.setProperty("/visualizacao", false);
+
+            },
+
+            aoCancelar: function(oEvent){
+
+                //resgata modelo inputs para alterar a propriedade /editavel
+                let oView = this.getView();
+
+                let oModeloInputs = oView.getModel("inputs");
+                oModeloInputs.setProperty("/editavel", false);
+
+                let oModeloBotoes = oView.getModel("botoes");
+                oModeloBotoes.setProperty("/edicao", false);
+                oModeloBotoes.setProperty("/visualizacao", true);
+            }
 
         });
     });
